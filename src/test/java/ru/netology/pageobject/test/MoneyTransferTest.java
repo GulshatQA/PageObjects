15a1468bf2f02636da08c1edc9b5f7dc6f8f7f42
Gulshat <gulshatkhamidulla313@gmail.com>
2024-04-20 16:25:07 +0500

    Initial commit

---
+++ src/test/java/ru/netology/pageobject/test/MoneyTransferTest.java
@@ -0,0 +1,13 @@
+package ru.netology.pageobject.test;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTest {
+
+    @Test
+    public void shouldTransferMoneyBetweenOwnCardV1() {
+        open("")
+    }
+}
